0818adf6e96bb95223a80e66343641a66c12a5c3
Tom Lodge <tlodge@dhcp-22-221.mrl.nottingham.ac.uk>
2013-02-22 12:18:11 +0000

    added files from postgresql modules

--- dataware-client_puppet/init.sh
+++ dataware-client_puppet/init.sh
@@ -5,5 +5,5 @@ sudo mkdir /usr/share/puppet
 sudo mkdir /usr/share/puppet/modules
 cd /usr/share/puppet/modules
 sudo ln -s /vagrant/puppet/dataware-client dataware-client
-sudo ln -s /vagrant/puppet/postgresql postgresql
+sudo ln -s /vagrant/puppet/postgres postgresql
 sudo -E puppet apply -e "include dataware-client" 
